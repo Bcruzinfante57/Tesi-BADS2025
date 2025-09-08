6089e5eaaf58c959f32a3931816239d5d87ccbde
Bcruzinfante57 <benjamincruzinfante@gmail.com>
2025-09-08 18:29:09 -0300

    Dolce&Gabban Inizio

--- Test D&G.py
+++ Test D&G.py
@@ -22,22 +22,35 @@ driver.maximize_window()
 
 time.sleep(3)
 
+## Accept Cookies
+try:
+        cookie_btn = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[normalize-space(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')) = 'aceptar todas las cookies']"))
+        )
+        cookie_btn.click()
+        print("Cookies accepted")
+except (TimeoutException, NoSuchElementException):
+        print("Cookie notice not found or different.")
+
+time.sleep(3)
+
    #. Scorrimento iniziale per rivelare il pulsante 'Carica Altro' **
 print("Desplazándose hacia abajo para revelar el botón 'CARICA ALTRO'...")
-
+last_height = driver.execute_script("return document.body.scrollHeight")
+products_count = 0
 while True:
     # To the bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(3) # Caricamento
 
     # Click CARICA ALTRO
-    pyautogui.moveTo(860, 330, duration=1.5) # Usa las coordenadas X e Y que has probado
+    pyautogui.moveTo(860, 550, duration=1.5) # Usa las coordenadas X e Y que has probado
     time.sleep(1)
     pyautogui.click(duration=0.5)
     time.sleep(5) # Attesa dopo il clic
 
     # Ottieni il numero attuale di prodotti sulla pagina utilizzando i selettori combinati
-    current_products = driver.find_elements(By.CSS_SELECTOR, "div.product-media.show-image")
+    current_products = driver.find_elements(By.CSS_SELECTOR, "div.ProductHit__product-hit--UcQZQ.product-hit")
     new_products_count = len(current_products)
 
     if new_products_count == products_count:
@@ -64,7 +77,7 @@ products_to_download = {}
 
 try:
     # 1. Encontrar todos los contenedores de productos
-    grid_items = driver.find_elements(By.CSS_SELECTOR, "div.product-media.show-image")
+    grid_items = driver.find_elements(By.CSS_SELECTOR, "div.ProductHit__product-hit--UcQZQ.product-hit")
     print(f"Número de contenedores de productos encontrados: {len(grid_items)}")
     
     # 2. Iterar sobre cada contenedor para encontrar la primera imagen
